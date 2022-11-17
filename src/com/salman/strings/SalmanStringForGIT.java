9626af77a7de61ec83ede742a5f09dc8b4551db9
algosal <pfc.salman@gmail.com>
2022-11-17 17:54:32 -0500

    String Array and GIT GUI

---
+++ src/com/salman/strings/SalmanStringForGIT.java
@@ -0,0 +1,13 @@
+package com.salman.strings;
+
+
+public class SalmanStringForGIT {
+	
+	private String[] s = new String[2];
+	
+	{		
+		s[0] =	"This is the GIT Class";
+		s[1] = "This is also an Array test";
+	
+	}
+}
